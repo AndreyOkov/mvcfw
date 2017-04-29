ea0819895d95bdb134754e3c8052439f3824caa7
Andrey Ovchinnikov <kukuruza-11@yandex.ru>
2017-04-29 15:52:41 +0300

    Сессии

--- webroot/index.php
+++ webroot/index.php
@@ -6,6 +6,8 @@ define('ROOT', dirname(dirname(__FILE__)));
 define('VIEWS_PATH', ROOT.DS.'views');
 require_once(ROOT.DS.'lib'.DS.'init.php');
 
+Session::setFlash('Test flash message');
+
 App::run($_SERVER['REQUEST_URI']);
 
-$test = App::$db->query('select * from pages');
+
