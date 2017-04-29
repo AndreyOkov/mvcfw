a5d2c6e889be88c57acb8ad4bc64822fd5700255
Andrey Ovchinnikov <kukuruza-11@yandex.ru>
2017-04-29 11:04:40 +0300

    Контроллеры

--- webroot/index.php
+++ webroot/index.php
@@ -6,14 +6,4 @@ define('ROOT', dirname(dirname(__FILE__)));
 
 require_once(ROOT.DS.'lib'.DS.'init.php');
 
-$router = new Router($_SERVER['REQUEST_URI']);
-var_dump($this);
-
-var_dump('Route: '.$router->getRoute());
-var_dump('Language : '.$router->getLanguage());
-var_dump('Controller : '.$router->getController());
-var_dump('Action to be called: '.$router->getMethodPrefix().$router->getAction());
-
-
-echo 'Params';
-var_dump($router->getParams());
+App::run($_SERVER['REQUEST_URI']);
